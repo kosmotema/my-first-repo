7411c694d1a1529aa95423e6cee7ed1034f03ef1
Anatoly Pitikin <git@xapdkop.dev>
2020-10-28 21:41:52 +0300

    Small rename and app rework

--- coffin_chick/MainWindow.xaml.cs
+++ coffin_chick/MainWindow.xaml.cs
@@ -13,29 +13,36 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Ящик
-{
+namespace CoffinChick {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-	{
+	public partial class MainWindow : Window {
 		public MainWindow() {
 			InitializeComponent();
+			TrayIcon.Visibility = Visibility.Visible;
+			Hide();
+			MessageBox.Show("Удачной игры в ящик :)", "Добро пожаловать!", MessageBoxButton.OK,
+				MessageBoxImage.Information);
 		}
 
 		private void MainWindow_OnStateChanged(object sender, EventArgs e) {
-			if (WindowState != WindowState.Minimized) return;
-			TrayIcon.Visibility = Visibility.Visible;
-			//ShowInTaskbar = true;
+			if (WindowState == WindowState.Minimized) return;
 			Hide();
+			MessageBox.Show("Не стоит разворачивать окно, не надо", "Будь осторожней", MessageBoxButton.OK,
+				MessageBoxImage.Warning);
 		}
 
-		private void TrayIcon_TrayLeftMouseDown(object sender, RoutedEventArgs e)
-		{
-			Show();
-			WindowState = WindowState.Normal;
-			TrayIcon.Visibility = Visibility.Hidden;
+		private void TrayIcon_TrayLeftMouseDown(object sender, RoutedEventArgs e) {
+			MessageBox.Show("Хочешь закрыть окно? Нажми правой кнопкой мыши по значку!", "А зачем?",
+				MessageBoxButton.OK,
+				MessageBoxImage.Information);
+		}
+
+		private void TrayIcon_TrayRightMouseDown(object sender, RoutedEventArgs e) {
+			if (MessageBox.Show("А ты точно хочешь выбраться из ящика?", "Не хочешь играть?", MessageBoxButton.YesNo,
+				MessageBoxImage.Warning) == MessageBoxResult.Yes)
+				Close();
 		}
 	}
 }
