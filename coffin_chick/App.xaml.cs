f5b91b4904720e7eec08f9cf0df5edb59ba7a3ad
Anatoly Pitikin <git@xapdkop.dev>
2020-10-30 22:22:24 +0300

    [v2] Remove MainWindow and move all code to App

--- coffin_chick/App.xaml.cs
+++ coffin_chick/App.xaml.cs
@@ -1,8 +1,26 @@
 ﻿using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace CoffinChick {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App : Application { }
+	public partial class App : Application {
+
+		public App() {
+			InitializeComponent();
+
+			((TaskbarIcon) Resources["TrayIcon"]).Visibility = Visibility.Visible;
+
+			MessageBox.Show(@"Удачной игры в ящик :)
+Чтобы вылезти из ящика, нажми на значок в трее", @"Добро пожаловать!", MessageBoxButton.OK,
+				MessageBoxImage.Information);
+		}
+
+		private void TrayIcon_TrayMouseDown(object sender, RoutedEventArgs e) {
+			if (MessageBox.Show(@"А ты точно хочешь выбраться из ящика?", @"Надоело играть?", MessageBoxButton.YesNo,
+				MessageBoxImage.Warning) == MessageBoxResult.Yes)
+				Shutdown();
+		}
+	}
 }
